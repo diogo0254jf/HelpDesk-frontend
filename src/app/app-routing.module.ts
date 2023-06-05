f14d2ab8e1fe2ab2844e958c35d693f54a357de5
Diogo Costa <46428002+diogo0254jf@users.noreply.github.com>
2023-06-05 16:08:53 -0300

    CSS alterado no frontend

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,14 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { AuthGuard } from "./auth/auth.guard";
+import { ChamadoCreateComponent } from "./components/chamado/chamado-create/chamado-create.component";
+import { ChamadoListComponent } from "./components/chamado/chamado-list/chamado-list.component";
+import { ChamadoReadComponent } from "./components/chamado/chamado-read/chamado-read.component";
+import { ChamadoUpdateComponent } from "./components/chamado/chamado-update/chamado-update.component";
+import { ClienteCreateComponent } from "./components/cliente/cliente-create/cliente-create.component";
+import { ClienteDeleteComponent } from "./components/cliente/cliente-delete/cliente-delete.component";
+import { ClienteListComponent } from "./components/cliente/cliente-list/cliente-list.component";
+import { ClienteUpdateComponent } from "./components/cliente/cliente-update/cliente-update.component";
 import { HomeComponent } from "./components/home/home.component";
 import { LoginComponent } from "./components/login/login.component";
 import { NavComponent } from "./components/nav/nav.component";
@@ -8,14 +16,6 @@ import { TecnicoCreateComponent } from "./components/tecnico/tecnico-create/tecn
 import { TecnicoDeleteComponent } from "./components/tecnico/tecnico-delete/tecnico-delete.component";
 import { TecnicoListComponent } from "./components/tecnico/tecnico-list/tecnico-list.component";
 import { TecnicoUpdateComponent } from "./components/tecnico/tecnico-update/tecnico-update.component";
-import { ClienteListComponent } from "./components/cliente/cliente-list/cliente-list.component";
-import { ClienteCreateComponent } from "./components/cliente/cliente-create/cliente-create.component";
-import { ClienteUpdateComponent } from "./components/cliente/cliente-update/cliente-update.component";
-import { ClienteDeleteComponent } from "./components/cliente/cliente-delete/cliente-delete.component";
-import { ChamadoListComponent } from "./components/chamado/chamado-list/chamado-list.component";
-import { ChamadoCreateComponent } from "./components/chamado/chamado-create/chamado-create.component";
-import { ChamadoUpdateComponent } from "./components/chamado/chamado-update/chamado-update.component";
-import { ChamadoReadComponent } from "./components/chamado/chamado-read/chamado-read.component";
 
 const routes: Routes = [
   { path: "login", component: LoginComponent },
